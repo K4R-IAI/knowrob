435fc030aec44d2aca4da376e303efac11b582a0
Giang Nguyen <hoanggia@uni-bremen.de>
2021-04-15 15:59:39 +0200

    Fix warnings

--- src/db/mongo/tripledb/triples.pl
+++ src/db/mongo/tripledb/triples.pl
@@ -936,7 +936,7 @@ get_scope_query2_(Query,'',[Operator,Value]) :-
 
 %%
 
-:- table(g_get_scope_document_/2).
+# :- table(get_scope_document_/2).
 get_scope_document_(Dict,List) :- get_scope_document_1(Dict,List).
 get_scope_document_1(Dict,List) :-
 	is_dict(Dict),
